3416f00e1e58c62fa48aee4efb8b706db22fe436
Jay S <allesyaj@gmail.com>
2019-11-29 21:39:24 -0500

    fix: site description

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
     title: `Spoon River Anthology`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    description: `A delve into the inter-connectivity of Edgar Lee Masters' "Spoon River Anthology."`,
+    // author: `@gatsbyjs`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
