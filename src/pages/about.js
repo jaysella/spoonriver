a51ba713788e01fa4103cef62bf4f4c5b39cfe01
Jay S <allesyaj@gmail.com>
2020-06-05 17:52:55 -0400

    feat: clean up about page, add contributing info

--- src/pages/about.js
+++ src/pages/about.js
@@ -12,7 +12,7 @@ const SecondPage = () => (
   <Layout hasDrawer="false">
     <SEO title="About the Project" />
 
-    <div className={['slide', styles.slide, styles.entry, styles.has_background].join(' ')}>
+    <div className={['slide slide--navigation', styles.slide, styles.entry, styles.has_background].join(' ')}>
       <Link to="/">
         <div className={styles.inner}>
           <h2>
@@ -37,7 +37,7 @@ const SecondPage = () => (
         <h2>
           Who is behind this project?
         </h2>
-        <p>This website was built for a December 2019 high school project conducted by Jay Sella, Oscar Lloyd, and Nick Casertano in the metropolitan Washington, D.C. area.</p>
+        <p>The concept, development, design, and initial data stemmed from a December 2019 high school project conducted by Nick Casertano, Oscar Lloyd, and Jay Sella in the metropolitan Washington, D.C. area.</p>
       </div>
     </div>
     <div className={['slide', styles.slide, styles.content].join(' ')}>
@@ -46,6 +46,14 @@ const SecondPage = () => (
           How was the data sourced?
         </h2>
         <p>All of the information contained on this site is opinion-based and formulated by the authors; it is not intended to be used for scholarly work. Use of this site constitutes the user's acknowledgement of these limitations.</p>
+      </div>
+    </div>
+    <div className={['slide', styles.slide, styles.content].join(' ')}>
+      <div className={styles.inner}>
+        <h2>
+          Can I contribute to this site?
+        </h2>
+        <p><b>Yes!</b> The source code + data for this site are available on <a href="//github.com/j-651/spoonriver" target="_blank" rel="noopener noreferrer">GitHub</a>. To contribute, open an issue or PR.</p>
       </div>
     </div>
 
