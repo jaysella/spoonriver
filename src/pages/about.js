fb67dbc05193c5de9525e0cca67a7c63cc37c6c1
Jay S <allesyaj@gmail.com>
2020-08-03 17:09:37 -0400

    fix: reenable about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -27,38 +27,50 @@ const SecondPage = () => (
         </div>
       </Link>
     </div>
-    {/* <div className={['slide', styles.slide, styles.content].join(' ')}>
+    <div className={["slide", styles.slide, styles.content].join(" ")}>
       <div className={styles.inner}>
-        <h2>
-          What is this project?
-        </h2>
-        <p>This is an English Literature project, tying together the overlapping elements between the narratives of Edgar Lee Masters' <em>Spoon River Anthology</em>.</p>
+        <h2>What is this project?</h2>
+        <p>
+          This is an English Literature project, tying together the overlapping
+          elements between the narratives of Edgar Lee Masters'{" "}
+          <em>Spoon River Anthology</em>.
+        </p>
       </div>
     </div>
-    <div className={['slide', styles.slide, styles.content].join(' ')}>
+    <div className={["slide", styles.slide, styles.content].join(" ")}>
       <div className={styles.inner}>
-        <h2>
-          Who is behind this project?
-        </h2>
-        <p>The concept, development, design, and initial data stemmed from a December 2019 high school project conducted by Nick Casertano, Oscar Lloyd, and Jay Sella in the metropolitan Washington, D.C. area.</p>
+        <h2>Who is behind this project?</h2>
+        <p>
+          The concept, development, design, and initial data stemmed from a
+          December 2019 high school project conducted by Nick Casertano, Oscar
+          Lloyd, and Jay Sella in Washington, D.C..
+        </p>
       </div>
     </div>
-    <div className={['slide', styles.slide, styles.content].join(' ')}>
+    <div className={["slide", styles.slide, styles.content].join(" ")}>
       <div className={styles.inner}>
-        <h2>
-          How was the data sourced?
-        </h2>
-        <p>All of the information contained on this site is opinion-based and formulated by the authors; it is not intended to be used for scholarly work. Use of this site constitutes the user's acknowledgement of these limitations.</p>
+        <h2>Can I contribute to this site?</h2>
+        <p>
+          <b>Yes!</b> The source code + data for this site are available on{" "}
+          <a
+            href="//github.com/j-651/spoonriver"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            GitHub
+          </a>
+          . To contribute, open an issue or PR (according to our{" "}
+          <a
+            href="https://github.com/j-651/spoonriver/blob/master/CONTRIBUTING.md"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Contribution Guidelines
+          </a>
+          ).
+        </p>
       </div>
     </div>
-    <div className={['slide', styles.slide, styles.content].join(' ')}>
-      <div className={styles.inner}>
-        <h2>
-          Can I contribute to this site?
-        </h2>
-        <p><b>Yes!</b> The source code + data for this site are available on <a href="//github.com/j-651/spoonriver" target="_blank" rel="noopener noreferrer">GitHub</a>. To contribute, open an issue or PR.</p>
-      </div>
-    </div> */}
   </Layout>
 )
 
