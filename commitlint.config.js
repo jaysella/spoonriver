3763f5ca757680ce2ff4ed41032476b7328c6434
Jay S <allesyaj@gmail.com>
2020-08-03 21:13:09 -0400

    devops: add husky, commitlint

---
+++ commitlint.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "header-max-length": [2, "always", 120],
+    "subject-case": [0],
+    "type-enum": [
+      2,
+      "always",
+      [
+        "feat",
+        "fix",
+        "docs",
+        "refactor",
+        "test",
+        "bump",
+        "chore",
+        "revert",
+        "ci",
+        "style",
+        "devops",
+      ],
+    ],
+    "scope-empty": [0],
+  },
+}
